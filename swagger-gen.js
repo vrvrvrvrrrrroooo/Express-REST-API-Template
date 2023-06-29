598d8a3c0f46807965b1f053d5ea3d5b3d926a3e
vrvrvrvrrrrroooo <138172510+vrvrvrvrrrrroooo@users.noreply.github.com>
2023-06-29 18:27:24 -0500

    Add files via upload

---
+++ swagger-gen.js
@@ -0,0 +1,26 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const outputFile = './swagger_output.json'
+const endpointsFiles = ['./app.js']
+
+// Optional and depends on security requirements for the API.
+const securityDefinitions = {
+	apiKeyAuth: {
+		type: 'apiKey',
+		in: 'header',
+		name: 'Authorization',
+	}
+};
+
+const doc = {
+	info: {
+		title: 'API Title',
+		description: 'API Description',
+	},
+	host: 'localhost:3000', // API's host
+	basePath: '/',
+	schemes: ['http'],
+	securityDefinitions: securityDefinitions,
+};
+
+swaggerAutogen(outputFile, endpointsFiles, doc)
